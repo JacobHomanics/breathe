4218313d5e9566061a5f97df0cfef6cc1f988c3c
Jacob Homanics <homanicsjake@gmail.com>
2025-02-19 23:01:23 -0500

    moved axes to variables

--- Assets/Scripts/Camera Controller/Offset Drag/CameraOffsetDrag.cs
+++ Assets/Scripts/Camera Controller/Offset Drag/CameraOffsetDrag.cs
@@ -15,23 +15,27 @@ public class CameraOffsetDrag : MonoBehaviour
     private void Calculate(Transform target)
     {
         if (Input.GetMouseButton(0))
-            Drag(target, userSettings.sensitivities, userSettings.invertYAxis, systemSettings.xRotationMethod, systemSettings.clamps);
+            Drag(target, userSettings.sensitivities, userSettings.xAxis, userSettings.yAxis, userSettings.invertXAxis, userSettings.invertYAxis, systemSettings.xRotationMethod, systemSettings.clamps);
     }
 
-    private void Drag(Transform target, Vector2 sensitivities, bool invertYAxis, CameraOffsetDragSystemSettingsScriptableObject.XRotationMethod xRotationMethod, Vector2 clamps)
+    private void Drag(Transform target, Vector2 sensitivities, string xAxis, string yAxis, bool invertXAxis, bool invertYAxis, CameraOffsetDragSystemSettingsScriptableObject.XRotationMethod xRotationMethod, Vector2 clamps)
     {
-        var yFrame = GetY(invertYAxis) * sensitivities.y * Time.deltaTime;
-        var xFrame = Input.GetAxis("Mouse X") * sensitivities.x * Time.deltaTime;
+        var x = Input.GetAxis(xAxis);
+        var y = Input.GetAxis(yAxis);
 
-        target.rotation = Quaternion.AngleAxis(xFrame, Vector3.up) * target.rotation;
+
+        var yDelta = invertYAxis ? -y : y * sensitivities.y * Time.deltaTime;
+        var xDelta = invertXAxis ? -x : x * sensitivities.x * Time.deltaTime;
+
+        target.rotation = Quaternion.AngleAxis(xDelta, Vector3.up) * target.rotation;
 
         if (xRotationMethod == CameraOffsetDragSystemSettingsScriptableObject.XRotationMethod.AngleAxis)
-            target.rotation = Quaternion.AngleAxis(yFrame, target.right) * target.rotation;
+            target.rotation = Quaternion.AngleAxis(yDelta, target.right) * target.rotation;
 
         var ea = target.rotation.eulerAngles;
 
         if (xRotationMethod == CameraOffsetDragSystemSettingsScriptableObject.XRotationMethod.EulerAngles)
-            ea.x += yFrame;
+            ea.x += yDelta;
 
         ea.x = ClampAngle(ea.x, clamps.x, clamps.y);
         target.eulerAngles = ea;
--- Assets/Scripts/Camera Controller/Offset Drag/CameraOffsetDragUserSettingsScriptableObject.cs
+++ Assets/Scripts/Camera Controller/Offset Drag/CameraOffsetDragUserSettingsScriptableObject.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Offset Drag - User Settings", menuName = "Scriptable Objects/Camera/Offset Drag/User Settings")]
 public class CameraOffsetDragUserSettingsScriptableObject : ScriptableObject
 {
+    public bool invertXAxis;
     public bool invertYAxis = true;
+
+    public string xAxis = "Mouse X";
+    public string yAxis = "Mouse Y";
 
     public Vector2 sensitivities;
 }
