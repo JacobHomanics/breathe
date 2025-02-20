c2853416e61b99f5a7cf7860afd6dffc2f63f30b
Jacob Homanics <homanicsjake@gmail.com>
2025-02-19 22:22:07 -0500

    removed commented out code

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,17 +3,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform cam;
-    public Transform target;
-
-    public CameraControllerSystemSettingsScriptableObject systemSettings;
     public CameraControllerUserSettingsScriptableObject userSettings;
-
-    public float Zoom { get; private set; }
-
-    void Start()
-    {
-        Zoom = userSettings.zoom;
-    }
 
     void LateUpdate()
     {
@@ -31,60 +21,6 @@ public class CameraController : MonoBehaviour
             targetPosition = components[i].Calculate(targetPosition);
         }
 
-        // targetPosition = HandleZoom(targetPosition);
-        // targetPosition = HandleBob(targetPosition, userSettings.periods, userSettings.amplitudes);
-        // targetPosition = cam.InverseTransformPoint(HandleObstacleCollision(cam.TransformPoint(targetPosition), target.position));
         cam.localPosition = Vector3.Lerp(cam.localPosition, targetPosition, Time.deltaTime * userSettings.smoothing);
-    }
-
-    private Vector3 HandleBob(Vector3 position, Vector3 periods, Vector3 amplitudes)
-    {
-        return new Vector3(
-            position.x + GetDistanceFromTheta(periods.x, amplitudes.x),
-            position.y + GetDistanceFromTheta(periods.y, amplitudes.y),
-            position.z + GetDistanceFromTheta(periods.z, amplitudes.z)
-        );
-    }
-
-    private float GetDistanceFromTheta(float period, float amplitude)
-    {
-        float theta = Time.timeSinceLevelLoad / period;
-        float distance = amplitude * Mathf.Sin(theta);
-
-        if (float.IsNaN(distance))
-            distance = 0;
-
-        return distance;
-    }
-
-    private Vector3 HandleZoom(Vector3 position)
-    {
-        return position += new Vector3(0, 0, -HandleZoom2());
-    }
-
-    private float HandleZoom2()
-    {
-        if (userSettings.invertZoomAxis)
-            Zoom -= Input.GetAxis(userSettings.zoomAxis) * userSettings.zoomSensitivity * Time.deltaTime;
-        else
-            Zoom += Input.GetAxis(userSettings.zoomAxis) * userSettings.zoomSensitivity * Time.deltaTime;
-
-        if (Zoom > userSettings.maxZoom)
-            Zoom = userSettings.maxZoom;
-
-        if (Zoom < systemSettings.minZoom)
-            Zoom = systemSettings.minZoom;
-
-        return Zoom;
-    }
-
-    private Vector3 HandleObstacleCollision(Vector3 start, Vector3 end)
-    {
-        if (Physics.Linecast(start, end, out RaycastHit hit, systemSettings.layerMask))
-        {
-            return new Vector3(hit.point.x, hit.point.y, hit.point.z);
-        }
-
-        return start;
     }
 }
